3f93d8349431f28ac0015dbb39175a02760fb7c1
siva <siva@ivs.tv>
2023-04-01 00:48:33 +0530

    some msg

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -22,7 +22,7 @@ locals {
 locals {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   web_security_group_id = data.aws_ssm_parameter.web_security_group_id.value
-  ecs_cluster_id = data.aws_ssm_parameter.ecs_cluster_id
-  web_target_group_arn = data.aws_ssm_parameter.web_target_group_arn
+  ecs_cluster_id = data.aws_ssm_parameter.ecs_cluster_id.value
+  web_target_group_arn = data.aws_ssm_parameter.web_target_group_arn.value
   private_subnets = split(",", data.aws_ssm_parameter.private_subnets.value)[0]
 }
