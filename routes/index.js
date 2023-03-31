059395d72aa1e40fd9108bdd394c8b4082dfc24c
siva <siva@ivs.tv>
2023-03-31 18:40:18 +0530

    some msg

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,21 @@ router.get('/', function(req, res, next) {
     );
 });
 
+
+router.get('/health', function(req, res, next) {
+    request({
+            method: 'GET',
+            url: api_url,
+            json: true
+        },
+        function(error, response, body) {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).send('error running request to ' + api_url);
+            } else {
+                res.send("OK");
+            }
+        }
+    );
+});
+
 module.exports = router;
